926bd31d66c8388a69a0c85fdc48023ededd3ed3
xtycoon <125221464+xtycoonfi@users.noreply.github.com>
2024-05-27 01:12:07 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -8,15 +8,15 @@ export const MONGODB_URI = getEnv('MONGODB_URI')
 
 import * as solWeb3 from "@solana/web3.js";
 // SOLANA MAINNET - NETWORK CONFIG
-//export const NETWORK = 'mainnet-beta'
-//export const RPC = 'https://lingering-damp-lambo.solana-mainnet.quiknode.pro/41ced52afd17c1798eb1b6524ae12a981521a1d4'
+export const NETWORK = 'mainnet-beta'
+export const RPC = 'https://lingering-damp-lambo.solana-mainnet.quiknode.pro/41ced52afd17c1798eb1b6524ae12a981521a1d4'
 //export const RPC = 'https://api.mainnet-beta.solana.com';
 
 // SOLANA DEVNET - NETWORK CONFIG
-export const NETWORK = 'devnet'
-export const RPC = 'https://api.devnet.solana.com'
+//export const NETWORK = 'devnet'
+//export const RPC = 'https://api.devnet.solana.com'
 
 export const SOLANA = new solWeb3.Connection(RPC, 'confirmed');
-export const PROGRAM_ID = '4hpHCxM1mT1TfrxPzcvQRYSkf2Jaf3nuTY6F1ZQEXZfB';
-export const ACCOUNT_BOT_PK = 'J2R22pquB6YC45oo6gyjMEnAztdQ8mXZ6sCXmBFn8Sdj'
-export const ACCOUNT_PROTOCOL_PK = '2LEHP4cHHYd1qWi21AgBMdPFHL9EbMKbk16hQzqnGtps'
+export const PROGRAM_ID = '2kptyUB75N4fZE7FfkMpC1qxoM6VWJ26HN95QFDgN9P4';
+export const ACCOUNT_BOT_PK = '3KW9UmB16bpCE7qayxgixXj43vXVqAwEZp5aeFxNSyGU'
+export const ACCOUNT_PROTOCOL_PK = 'sr8S4uYAcCvgDWBr1QHsTNU5ftf8NR4JakSb7QShBUh' 
